69eff02310b2d73991e5bf0d6772eeddea0a6887
Houssem Amri <house.mail.md@gmail.com>
2021-06-22 17:25:26 +0200

    Add files via upload

--- ubuntu1604.sh
+++ ubuntu1604.sh
@@ -19,6 +19,15 @@ set -e
 #
 # Dependencies
 #
+apt install -y software-properties-common
+apt-add-repository ppa:brightbox/ruby-ng -y
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirrors.coreix.net/mariadb/repo/10.1/ubuntu xenial main'
+curl -sL https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -
+add-apt-repository 'deb http://www.rabbitmq.com/debian/ testing main'
+export DEBIAN_FRONTEND=noninteractive
+apt install -y ruby2.3 ruby2.3-dev build-essential libssl-dev mariadb-server libmysqlclient-dev rabbitmq-server nodejs git nginx wget nano
+gem install bundler procodile --no-rdoc --no-ri
 
 #
 # MySQL
